54530f8b15a71b9c4945c07e65dff735209168d0
HoldinMcGroins <113585933+HoldinMcGroins@users.noreply.github.com>
2024-08-21 07:29:15 +0800

    frontend setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import Navbar from './features/Navbar'
+function App() {
+
+  return (
+    <>
+      <div>
+        <Navbar/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/features/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+import "../styles/Navbar.css"
+function Navbar() {
+  return (
+    <div>
+        <Typography className='text heading stats'>STATS</Typography>
+        <Typography className='text heading skills'>SKILLS</Typography>
+        <Typography className='text heading game'>GAME</Typography>
+        <Typography className='text heading game'>REFER</Typography>
+        <Typography className='text heading wallet'>WALLET</Typography>
+        
+    </div>
+  )
+}
+
+export default Navbar
