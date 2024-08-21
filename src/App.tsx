6ff1252dd2b42cbb77adae029991f60cb784f81b
HoldinMcGroins <113585933+HoldinMcGroins@users.noreply.github.com>
2024-08-22 00:46:02 +0800

    wallet

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,29 @@ import { useState } from 'react'
 
 import './App.css'
 import Navbar from './features/Navbar'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom'
+
+import Wallet from './pages/Wallet'
 function App() {
 
   return (
     <>
       <div className="navbar-main">
+       
         <Navbar />
       </div>
+      <Router>
+          <Routes>
+            {/* <Route path="/home" element={<Homepage />} />
+            <Route path="/" element={<Homepage />} /> */}
+            <Route path="/wallet" element={<Wallet />} />
+            
+          </Routes>
+        </Router>
     </>
   )
 }
---
+++ src/pages/Wallet.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import "../styles/Wallet.css"
+import axios from 'axios'
+import hand from "../assets/hand.png"
+import bubble from "../assets/bubble.png"
+import { Button } from '@mui/material'
+function Wallet() {
+    const [bubbleEarned] = useState(3209)
+    const handleConnectWallet = async () => {
+        const address = 'your_wallet_address'; // Replace this with the actual address value
+        try {
+            const response = await axios.post("http://localhost:9000/connect", {
+                address,
+            });
+            console.log('Response:', response.data); // Handle the response as needed
+        } catch (error) {
+            console.error('Error connecting wallet:', error);
+        }
+    };
+    return (
+        <div style={{ width: "350px" }}>
+            <div className='wallet-top-container'>
+                <p className='text'>When the bubble disappears,</p>
+                <p className='text large noLH'>$Bubble</p>
+                <p className='text'>will list on TON</p>
+                <img src={hand} alt="" />
+            </div>
+            <div className='wallet-middle-container'>
+                <div className='bubble-section'>
+                    <img src={bubble} alt="" />
+                    <div style={{ paddingLeft: "0.5rem" }}>
+                        <p className='text noLH bubble-earned-text'>{bubbleEarned}</p>
+                        <p className='text noLH'>$Bubble</p>
+                        <p className='text noLH'>Earned</p>
+                    </div>
+                </div>
+                <div className='bubble-information-container'>
+                    <p className='text small'>$BUBBLE will be airdropped 1:1 with bubble </p>
+                    <p className='text small'>earned when the bubble disappears</p>
+                    <p className='text small'>Increase bubble earnings by leveling up</p>
+                </div>
+                <div className='connect-btn-div'>
+                    <Button className='connect-wallet text' onClick={handleConnectWallet}>
+                        Connect Wallet
+                    </Button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Wallet
